da9bfa9cb9f28dbcd0b673d8b41257128ffefaa1
codekid <kletts2000@gmail.com>
2017-05-13 15:12:26 -0400

    Added additional exercise

---
+++ draw_game_board.py
@@ -0,0 +1,21 @@
+# Print a game board of size NxN 
+
+def printBoard(size):
+
+	print " --- " * size
+
+	for i in range(0,size):
+
+		print "|    " * (size-1) + "|   |"
+		print " --- " * size
+
+
+def main():
+
+	print "Please enter what size game board you want"
+	size = int(raw_input())
+	printBoard(size)
+
+
+if __name__ == '__main__':
+	main()
